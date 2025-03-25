199eaf549bcb9f6e46be61aa351b7aa10972be18
Himanshi Kumari <himanshisonkar17@gmail.com>
2025-03-26 00:50:03 +0530

    Removed comments from JS file

--- app.js
+++ app.js
@@ -1,32 +1,6 @@
-// let body=document.querySelector('body');
-// let input=document.querySelector('input');
-// let add=document.querySelector('#add');
-// let list=document.querySelector("ul");
-
-// body.appendChild(list);
-
-// add.addEventListener('click',function(){
-//     let li=document.createElement('li');
-//     let remove=document.createElement('button');
-//     remove.innerText="remove";
-//     remove.classList.add("delete");
-//     li.innerHTML=input.value;
-//     li.appendChild(remove);
-//     list.appendChild(li);
-//     input.value="";
-// });
-
-// let removed= document.querySelectorAll(".delete");
-// for(remove of removed){
-//         remove.addEventListener('click',function(){
-//         console.log("task Removed");
-//     }); 
-// }
-
 let button=document.querySelector('button');
 let ul=document.querySelector('ul');
 let input=document.querySelector('input');
-// let remove=document.querySelectorAll('.delete');
 
 button.addEventListener('click',function(){
     let li=document.createElement('li');
@@ -49,12 +23,3 @@ ul.addEventListener('click',function(event){
         console.log("task Deleted");
     }
 });
-// let removed=document.querySelectorAll('.delete');
-// for(remove of removed){
-//     remove.addEventListener('click',function(){
-//         let prnt=this.parentElement;
-//         console.log(prnt);
-//         prnt.remove();
-//         console.log('task removed');
-//     });
-// }  
